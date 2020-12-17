f660a111381bbe67d69d14c7c85e1b9049c3cfd4
Igor Oliveira Vilneck <igor@appelsoft.com.br>
2020-12-17 19:38:00 -0300

    ajuste

--- app/config/communication.php
+++ app/config/communication.php
@@ -1,9 +1,9 @@
 <?php 
 return[
-    'host' => "localhost",
-    'name' => "sistema_rh",
-    'user' => "root",
-    'pass' => "",
+    'host' => "psicologiarh.com.br",
+    'name' => "psicolog_sistema_principal",
+    'user' => "psicolog_user",
+    'pass' => "sistema_rh_2020",
     'type' => "mysql",
     'prep' => "1",
     'slog' => "SystemSqlLogService"
